ca8aa6c40bce5c74fc21525a195e2215f2223326
Matthew Simpson <matthew@matthewsimpson.net>
2022-01-01 20:22:53 +0000

    Show correct characters

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-// use colored::*;
+use std::io::{stdin};
+use colored::*;
 
 fn main() {
-    let word = String::from("HELLO");
-
-    // print!("{}", "H".green());
-    // print!("E");
-    // print!("L");
-    // print!("L");
-    // print!("O");
-    // println!();
+    const WORD_LENGTH :usize = 5;
+    let word = String::from("WORLD");
 
     let mut guess = String::new();
 
-    println!("H....");
+    println!("{}....", String::from(word.chars().nth(0).unwrap()).green());
 
     while guess.ne(&word) {
-        if guess.ne("") {
-            println!("Nope");
-        }
         guess = String::new();
-        std::io::stdin().read_line(&mut guess).expect("Could not read input");
-        guess = guess[0..5].into();
-        println!("Guess: {}", guess);
+        stdin().read_line(&mut guess).expect("Could not read input");
+
+        guess = guess.trim()[0..WORD_LENGTH].to_uppercase();
+
+        for (i, char) in guess.chars().enumerate() {
+            let print_char = match char.eq(&word.chars().nth(i).unwrap()) {
+                true => String::from(char).green().bold(),
+                false => String::from(char).normal(),
+            };
+            print!("{}", print_char);
+        }
+        println!();
     }
 
     println!("Correct! Well done!")
