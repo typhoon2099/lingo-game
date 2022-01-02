2368564afce1a5a93c317491c5c1873feb0e16ce
Matthew Simpson <matthew@matthewsimpson.net>
2022-01-02 13:59:15 +0000

    Correct display of characters in incorrect position

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,47 @@ fn main() {
 
         guess = guess[0..WORD_LENGTH].to_uppercase();
 
+        let mut matches: [i8; WORD_LENGTH] = [0; WORD_LENGTH];
+
         for (i, char) in guess.chars().enumerate() {
-            let print_char = match char.eq(&word.chars().nth(i).unwrap()) {
-                true => String::from(char).green().bold(),
-                false => String::from(char).normal(),
+            let exact_match = char.eq(&word.chars().nth(i).unwrap());
+
+            if exact_match {
+                matches[i] = 2;
+            }
+        }
+
+        for (i, char) in guess.chars().enumerate() {
+            if matches[i] != 0 {
+                // Stop checking if we've already found an exact match
+                continue;
+            }
+
+            let positions: Vec<usize> = word.match_indices(char).map(|(position, _str)| {
+                position
+            }).collect();
+
+            for position in positions {
+                if matches[position] != 0 {
+                    // Stop if we've already found a match (full or partial)
+                    break;
+                }
+
+                matches[i] = 1;
+            }
+        }
+
+        for (i, char) in guess.chars().enumerate() {
+            let print_char = match matches[i] {
+                1 => char.to_string().yellow(),
+                2 => char.to_string().green(),
+                _ => char.to_string().normal(),
             };
+
             print!("{}", print_char);
         }
-        println!();
+
+        println!()
     }
 
     println!("Correct! Well done!")
